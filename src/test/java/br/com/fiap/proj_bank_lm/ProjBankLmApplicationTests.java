1b845c09b04c823e05b23f2973c492db430d52b5
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-03-13 10:31:38 -0300

    first commit

---
+++ src/test/java/br/com/fiap/proj_bank_lm/ProjBankLmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.proj_bank_lm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjBankLmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
